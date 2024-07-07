87c1b8c393167ef1453a9ee1148ffa69e0917ea7
God_Lights <backup.xtapu3@gmail.com>
2024-07-07 19:58:40 +0900

    .

--- app/src/main/java/net/xtapu/android/kbo/Kiwoom.java
+++ app/src/main/java/net/xtapu/android/kbo/Kiwoom.java
@@ -1,7 +1,10 @@
 package net.xtapu.android.kbo;
 
+import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
 
 public class Kiwoom extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class Kiwoom extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kiwoom);
+    }
+    public void back(View v) {
+        Snackbar.make(v,"개발중",Snackbar.LENGTH_SHORT).show();
     }
 }
