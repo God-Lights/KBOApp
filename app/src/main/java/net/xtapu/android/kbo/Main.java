da0b3a3a9df098b82c46550900ca7d3117f1aed3
God_Lights <backup.xtapu3@gmail.com>
2024-07-07 19:10:54 +0900

    .

--- app/src/main/java/net/xtapu/android/kbo/Main.java
+++ app/src/main/java/net/xtapu/android/kbo/Main.java
@@ -1,8 +1,10 @@
 package net.xtapu.android.kbo;
 
+import android.content.Intent;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
 
 public class Main extends AppCompatActivity {
 
@@ -12,33 +14,34 @@ public class Main extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void buttonKiwoomPressed(View v) {
-
+        Intent intent = new Intent(getApplicationContext(), Kiwoom.class);
+        startActivity(intent);
     }
     public void buttonKtPressed(View v) {
-
+        Snackbar.make(v,"개발중",Snackbar.LENGTH_SHORT).show();
     }
     public void buttonSSGPressed(View v) {
-
+        Snackbar.make(v,"개발중",Snackbar.LENGTH_SHORT).show();
     }
     public void buttonHanwhaPressed(View v) {
-
+        Snackbar.make(v,"개발중",Snackbar.LENGTH_SHORT).show();
     }
     public void buttonLgPressed(View v) {
-
+        Snackbar.make(v,"개발중",Snackbar.LENGTH_SHORT).show();
     }
     public void buttonDoosanPressed(View v) {
-
+        Snackbar.make(v,"개발중",Snackbar.LENGTH_SHORT).show();
     }
     public void buttonKiaPressed(View v) {
-
+        Snackbar.make(v,"개발중",Snackbar.LENGTH_SHORT).show();
     }
     public void buttonLottePressed(View v) {
-
+        Snackbar.make(v,"개발중",Snackbar.LENGTH_SHORT).show();
     }
     public void buttonNcPressed(View v) {
-
+        Snackbar.make(v,"개발중",Snackbar.LENGTH_SHORT).show();
     }
     public void buttonSamsungPressed(View v) {
-
+        Snackbar.make(v,"개발중",Snackbar.LENGTH_SHORT).show();
     }
 }
